8da063826bfcb5e82bde62bfa2b8859c039b5675
asep.rojali <asep.rojali@pajakku.com>
2023-11-29 17:22:19 +0700

    fix java 21

--- src/test/java/io/github/avew/CsvSftpValue.java
+++ src/test/java/io/github/avew/CsvSftpValue.java
@@ -1,10 +1,8 @@
 package io.github.avew;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Builder
 @NoArgsConstructor
